838bb55b2986fc218c92175eecb086aa59a39677
Donald Fox <fox_donald@yahoo.com>
2020-04-05 14:59:26 -0400

    Added axios request

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
     onSearchSubmit(term) {
-        console.log(term);
+        axios
+            .get('https://api.unsplash.com/search/photos', {
+                params: { query: term },
+                headers: {
+                    Authorization: 'Client-ID YZyUJCUcuE3L6G4ZHnCcbWHson-Y42U7vcsLdxihFRk',
+                },
+            })
+            .then((response) => {
+                console.log(response.data.results);
+            });
     }
 
     render() {
