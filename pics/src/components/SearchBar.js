3a2b28ce93f2120eefeb92616590cae06e041a39
Donald Fox <fox_donald@yahoo.com>
2020-04-04 21:37:06 -0400

    Context, communicating child to parent, and callbacks

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -3,11 +3,11 @@ import React from 'react';
 class SearchBar extends React.Component {
     state = { term: '' };
 
-    onFormSubmit(event) {
+    onFormSubmit = (event) => {
         event.preventDefault();
 
-        console.log(this.state.term);
-    }
+        this.props.onSubmit(this.state.term);
+    };
 
     render() {
         return (
@@ -18,7 +18,7 @@ class SearchBar extends React.Component {
                         <input
                             type='text'
                             value={this.state.term}
-                            onChange={e => this.setState({ term: e.target.value })}
+                            onChange={(e) => this.setState({ term: e.target.value })}
                         />
                     </div>
                 </form>
