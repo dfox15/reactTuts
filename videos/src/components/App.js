9e656d5bc3d3abb7d6d792e2a2f07cef560d3d18
Donald Fox <fox_donald@yahoo.com>
2020-04-11 16:35:55 -0400

    Implement VideoItem component

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -6,7 +6,7 @@ import youtube from '../apis/youtube'
 const KEY = process.env.REACT_APP_YOUTUBE_API
 
 class App extends React.Component {
-    state = { videos: [] }
+    state = { videos: [], selectedVideo: null }
 
     onTermSubmit = async (term) => {
         const response = await youtube.get('/search', {
@@ -22,11 +22,15 @@ class App extends React.Component {
         this.setState({ videos: response.data.items })
     }
 
+    onVideoSelect = (video) => {
+        console.log('From the App!', video)
+    }
+
     render() {
         return (
             <div className='ui container'>
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                <VideoList videos={this.state.videos} />
+                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
             </div>
         )
     }
