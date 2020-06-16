694ced6b3add0dfbaf8fa709a7c8c6fae51d143c
dfox15 <donald.fox@gmail.com>
2020-06-16 13:50:46 -0400

    Revert "Full stack node"

--- fullStackNode/server/config/dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    googleClientID: '639889150631-crnoknh7762anrsv9g3ofob684e6qsug.apps.googleusercontent.com',
-    googleClientSecret: '778kMNg7HA6hXLHD9gSuidC8',
-    mongoURI:
-        'mongodb+srv://dfoxProd:f4L7inJKBLFg9KND@cluster0-sfwaw.mongodb.net/emaily-prod?retryWrites=true&w=majority',
-    cookieKey: 'FSADFUS0DEUF54TU5989E8RFDSAFRE'
-}
